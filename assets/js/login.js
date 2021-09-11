28bd6d2b9b89bd32e73bca8f8492955742c1b4c2
huanghuayue <1138691207@qq.com>
2021-09-11 23:50:24 +0800

    完成登录和注册的功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,124 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
+$(function(){
+  // 点击去注册
+  $('#link_reg').on('click',function(){
+    $('.login-box').hide();
+    $('.reg-box').show();
   })
-
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
+  // 点击登录
+  $('#link_login').on('click',function(){
+    $('.login-box').show();
+    $('.reg-box').hide();
   })
-
-  // 从 layui 中获取 form 对象
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+  // 校验表单
+  // 从layui中获取form对象
+  let form = layui.form;
+  // 从layui中获取弹出框对象
+  let layer = layui.layer;
+  // 通过from.verify()函数自定义校验规矩
   form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
-        return '两次密码不一致！'
+    // 自定义一个叫pwd的校验规则
+    // 数组的方式检验规则
+    pwd:[/^[\S]{6,12}$/,'密码必须6-12位，且不能为空'],
+    // 校验两次密码是否一致
+    // 方法的方式检验规则
+    repwd: function(value){
+      // value拿到的是确认密码的内容
+      // 获取密码的内容
+      let pwd = $('.reg-box [name=password]').val()
+      if(pwd !== value){
+        return '两次密码不一致'
       }
     }
   })
+  // 监听注册表单提交事件
 
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
-    })
-  })
+  // $('#from_reg').on('submit',function(e){
+  //   e.preventDefault();//阻止默认行为
+  //   $.post('http://ajax.frontend.itheima.net/api/reguser',
+  //   {
+  //     username:$('#from_reg [name=username]').val(),
+  //     password:$('#from_reg [name=password]').val()
+  //   },function(res){
+  //     if(res.status !== 0){
+  //       // 注册失败，错误提示
+  //       return layer.msg(res.message);
+  //     }
+  //     layer.msg('注册成功');
+  //     // 注册成功之后跳转会登录页面
+  //     $('#link_login').click()
+  //   })
+  // })
 
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
-    e.preventDefault()
-    $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('登录失败！')
+  // 监听登录事件
+
+  // $('#form_login').submit(function(e){
+  //   e.preventDefault();//阻止默认行为
+  //   $.ajax({
+  //     url:'http://ajax.frontend.itheima.net/api/login',
+  //     method:'POST',
+  //     // 快速获取表单中的数据
+  //     data:$(this).serialize(),
+  //     success:function(res){
+  //       if(res.status !== 0){
+  //         // 登录失败，错误提示
+  //         return layer.msg(res.message);
+  //       }
+  //       layer.msg('注册成功');
+  //       console.log(res.token);
+  //       localStorage.setItem('token',res.token);
+  //       // 登录成功之后跳转到首页
+  //       location.href = '/index.html';
+  //     }
+  //   })
+  // })
+
+  // 本地注册和登录
+  
+  $('#zhuce').on('click',function(e){
+    e.preventDefault();
+    let userinfo = JSON.parse(localStorage.getItem('userinfo'));
+    let infolist = [];
+    let isShow = true;
+    let username = $('#from_reg [name=username]').val();
+    let password = $('#from_reg [name=password]').val();
+    if(userinfo){
+      infolist = userinfo;
+      userinfo.forEach(item =>{
+        if(item.username == username){
+          isShow = false;
+          return layer.msg("用户名已存在");
         }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
-      }
-    })
+      })
+    }
+    if(isShow && username !== '' && password !== ''){
+      infolist.push({username:username,password:password})
+      localStorage.setItem('userinfo',JSON.stringify(infolist));
+      layer.msg("注册成功");
+      // 跳转到登录界面
+      $('#link_login').click();
+    }
   })
+
+  $('#denglu').on('click',function(e){
+    e.preventDefault();
+    let userinfo = JSON.parse(localStorage.getItem('userinfo'));
+    let username = $('#form_login [name=username]').val();
+    let password = $('#form_login [name=password]').val();
+    if(userinfo){
+      userinfo.forEach(item =>{
+        if(item.username != username){
+          return layer.msg('用户不存在');
+        }else if(item.password != password){
+          return layer.msg('用户名或者密码错误');
+        }else if(item.username == username && item.password == password){
+          localStorage.setItem('token','tokeninfo');
+          return layer.msg('登录成功');
+          location.href = '/index.html';
+        }
+      })
+    }else{
+      return layer.msg('用户不存在');
+    }
+  })
+
 })
